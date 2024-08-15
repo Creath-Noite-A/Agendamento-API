8369268d62f849bedf24002f14f4d83217b80613
Levi Sales <levisls.inbox@gmail.com>
2024-08-14 23:51:36 -0300

    update: CriarUsuario

--- src/app/usecases/criarUsuario.ts
+++ src/app/usecases/criarUsuario.ts
@@ -1,3 +1,12 @@
-export default class CriarUsuário {
-    
+import Usuario from "../../domain/models/usuario";
+import IRepositorioUsuario from "../gateways/iRepositorioUsuario"
+
+export default class CriarUsuario {
+    #repositorio: IRepositorioUsuario;
+    constructor(repositorio: IRepositorioUsuario) {
+        this.#repositorio = repositorio
+    }
+    public cadastrar(usuario: Usuario): Usuario {
+        return this.#repositorio.cadastrarUsuario(usuario);
+    }
 }
