704142488d7704a3d33750f8efdf0dd133d1b8ba
Caio Alves <caioanegocios@gmail.com>
2024-08-18 23:28:23 -0300

    update: iGatewayUsuario

--- src/app/gateways/iGatewayUsuario.ts
+++ src/app/gateways/iGatewayUsuario.ts
@@ -1,6 +1,6 @@
 import Usuario from '../../domain/models/usuario';
 
 export default interface IGatewayUsuario {
-    cadastrarUsuario(usuario: Usuario): Usuario;
-    listarUsuarios(): Array<Usuario>;
+    cadastrarUsuario(usuario: Usuario): Promise<Usuario>;
+    listarUsuarios(): Promise<Array<Usuario>>;
 }
