6f4cf388a6373f7532a2cd9a1e430f640bbd78a4
Levi Sales <levisls.inbox@gmail.com>
2024-08-20 08:42:06 -0300

    remove: adaptando arquitetura de código

--- src/app/gateways/iGatewayUsuario.ts
+++
@@ -1,6 +0,0 @@
-import Usuario from '../../domain/models/usuario';
-
-export default interface IGatewayUsuario {
-    cadastrarUsuario(usuario: Usuario): Usuario;
-    listarUsuarios(): Array<Usuario>;
-}
