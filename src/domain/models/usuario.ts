28713889865f58d44be4c410fd6be4c69f063cab
Levi Sales <levisls.inbox@gmail.com>
2024-08-20 08:42:25 -0300

    update: adição de novas estruturas

--- src/domain/models/usuario.ts
+++ src/domain/models/usuario.ts
@@ -1,45 +1,13 @@
 export default class Usuario {
-    #id: string;
-    #telefone: number;
-    #nome: string;
-    #senha: string
+    readonly id: string;
+    readonly telefone: number;
+    readonly nome: string;
+    readonly senha: string
 
-    constructor(id: string, telefone: number, nome: string, senha: string) {
-        this.#id = id;
-        this.#telefone = telefone;
-        this.#nome = nome;
-        this.#senha = senha;
-    }
-
-    get id(): string {
-        return this.#id;
-    }
-
-    set id(value: string) {
-        this.#id = value;
-    }
-
-    get telefone(): number {
-        return this.#telefone;
-    }
-
-    set telefone(value: number) {
-        this.#telefone = value;
-    }
-
-    get nome(): string {
-        return this.#nome;
-    }
-
-    set nome(value: string) {
-        this.#nome = value;
-    }
-
-    get senha(): string {
-        return this.#senha;
-    }
-
-    set senha(value: string) {
-        this.#senha = value;
+    public constructor(id: string, telefone: number, nome: string, senha: string) {
+        this.id = id;
+        this.telefone = telefone;
+        this.nome = nome;
+        this.senha = senha;
     }
 }
